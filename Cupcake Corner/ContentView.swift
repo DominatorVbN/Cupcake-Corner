8dde61d24a8748652f942cd72f6f6ff1ee4a2285
Dominator <amitsamant@outlook.com>
2019-12-15 13:00:53 +0530

    Loaded data from network in swift ui

--- Cupcake Corner/ContentView.swift
+++ Cupcake Corner/ContentView.swift
@@ -7,10 +7,42 @@
 //
 
 import SwiftUI
-
+struct Response: Codable{
+    let results : [Result]
+}
+struct Result: Codable{
+    let trackId: Int
+    let trackName: String
+    let collectionName: String
+}
 struct ContentView: View {
+    @State var results = [Result]()
     var body: some View {
-        Text("Hello, World!")
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                Text(item.collectionName)
+            }
+        }.onAppear(perform: loadData)
+    }
+    func loadData(){
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else{
+            print("invalid URl")
+            return
+        }
+        let request = URLRequest(url: url)
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data{
+                if let decodedData = try? JSONDecoder().decode(Response.self, from: data){
+                    DispatchQueue.main.async{
+                        self.results = decodedData.results
+                    }
+                    return
+                }
+            }
+            print("error",error as Any)
+        }.resume()
     }
 }
 
